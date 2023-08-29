47e07a2923eb03b06f04573d32ae23c58b088f7f
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-08-29 12:46:44 +0200

    folderstructure

--- api/controller/index.js
+++ api/controller/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const {verifyAToken} = require('../middleware/authentication')
 const routes = express.Router()
 //Import all model's objects
-const {users} = require('../model')
+const {users, books} = require('../model')
 //User's router
 routes.get('/users', (req, res)=>{
     users.fetchUsers(req, res)
@@ -31,7 +31,50 @@ bodyParser.json(), (req, res)=>{
     users.login(req, res)
 })
 //Book's router
-// routes.get(/book)
+routes.get('/books', (req, res)=>{
+    books.fetchbooks(req, res)
+})
+routes.get('/books/:id', (req, res)=>{
+    books.fetchbooks(req, res)
+})
+routes.post('/register',bodyParser.json(),
+(req, res)=>{
+    books.register(req, res)
+})
+routes.put('/books/:id', bodyParser.json(),
+ (req, res)=>{
+    books.updatebooks(req,res)
+})
+routes.patch('/books/:id', bodyParser.json(),
+ (req, res)=>{
+    books.updatebooks(req,res)
+})
+routes.delete('/books/:id', (req, res)=>{
+    books.delete(req, res)
+})
+//ODER ROUTES
+routes.get('/oders', (req, res)=>{
+    orders.fetchorders(req, res)
+})
+routes.get('/orders/:id', (req, res)=>{
+    orders.fetchorders(req, res)
+})
+routes.post('/add',bodyParser.json(),
+(req, res)=>{
+    orders.add(req, res)
+})
+routes.put('/orders/:id', bodyParser.json(),
+ (req, res)=>{
+    orders.updateorders(req,res)
+})
+routes.patch('/orders/:id', bodyParser.json(),
+ (req, res)=>{
+    orders.updateorders(req,res)
+})
+routes.delete('/orders/:id', (req, res)=>{
+    orders.delete(req, res)
+})
+// Book Author
 
 
 
